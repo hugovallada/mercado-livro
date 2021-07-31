144f1056acd2a6728018d659ebb5f83685fdba04
hugovallada <valladahugo@gmail.com>
2021-07-31 20:47:48 -0300

    Adiciona a opção de ignorar campos nulos na exibição do json de resposta

--- src/main/kotlin/com/github/hugovallada/mercadolivro/client/api/CustomerApi.kt
+++ src/main/kotlin/com/github/hugovallada/mercadolivro/client/api/CustomerApi.kt
@@ -2,9 +2,11 @@ package com.github.hugovallada.mercadolivro.client.api
 
 import com.github.hugovallada.mercadolivro.client.model.CustomerRequest
 import com.github.hugovallada.mercadolivro.client.model.CustomerResponse
+import com.github.hugovallada.mercadolivro.client.model.MessageResponse
 import io.swagger.annotations.ApiOperation
 import io.swagger.annotations.ApiResponse
 import io.swagger.annotations.ApiResponses
+import org.aspectj.bridge.Message
 import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
@@ -18,9 +20,9 @@ interface CustomerApi {
     @ApiOperation(value = "Creates new customer", notes = "Creates new customer on the database")
     @ApiResponses(value = [
         ApiResponse(code = 201, message = "Created", response = CustomerResponse::class),
-        ApiResponse(code = 400, message = "Bad Request"),
-        ApiResponse(code = 422, message = "Already Exists"),
-        ApiResponse(code = 500, message = "Internal Server Error")
+        ApiResponse(code = 400, message = "Bad Request", response = MessageResponse::class),
+        ApiResponse(code = 422, message = "Already Exists", response = MessageResponse::class),
+        ApiResponse(code = 500, message = "Internal Server Error", response = MessageResponse::class)
     ])
     @ResponseStatus(HttpStatus.CREATED)
     @PostMapping
